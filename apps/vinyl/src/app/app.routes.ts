32aa2d39d176ec731e74718e9dec4a7fd236e3da
Gytis Lazauskas <gytis@Gytiss-MacBook-Pro.local>
2023-11-25 11:22:44 +0200

    boilerplate-cleanup

--- apps/vinyl/src/app/app.routes.ts
+++ apps/vinyl/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { HomepageComponent } from '@vinyl-mono/homepage';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '**',
+    component: HomepageComponent
+  }
+];
